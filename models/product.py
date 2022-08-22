1fb4203d436c77740dd01d4329237957dcec7137
robin-cybrosys <robin.cybrosys@gmail.com>
2022-08-22 16:47:53 +0530

    Aug 22:Task2 (pos rating) completed

--- models/product.py
+++ models/product.py
@@ -1,7 +1,25 @@
-from odoo import fields, models
+from odoo import fields, models, api
+
+AVAILABLE_PRIORITIES = [
+    ('0', 'Nil'),
+    ('1', 'Normal'),
+    ('2', 'Average'),
+    ('3', 'Good'),
+    ('4', 'Very Good'),
+    ('5', 'Excellent'),
+]
 
 
 class Product(models.Model):
     _inherit = ['product.template']
     grade = fields.Char(string="Grade")
+    rating = fields.Selection(AVAILABLE_PRIORITIES, default='0')
 
+    @api.onchange('rating')
+    def change(self):
+        print(self.rating)
+    # ('1', '1'),
+    # ('2', '2'),
+    # ('3', '3'),
+    # ('4', '4'),
+    # ('5', '⭐⭐⭐⭐')],index=True)
--- static/src/js/grade.js
+++ static/src/js/grade.js
@@ -5,7 +5,7 @@
     models.PosModel = models.PosModel.extend({
         initialize: function(session, attributes){
             var self = this;
-            models.load_fields('product.product', ['grade']);
+            models.load_fields('product.product', ['grade','rating']);
             _super_product.initialize.apply(this, arguments);
         }
     });
@@ -15,13 +15,15 @@ odoo.define('module_name.receipt', function(require){
     "use strict";
     console.log('start');
     var models = require('point_of_sale.models');
-    models.load_fields('product.product', 'grade');
+    models.load_fields('product.product', ['grade','rating']);
     var _super_orderline = models.Orderline.prototype;
     models.Orderline = models.Orderline.extend({
         export_for_printing: function(){
             var line = _super_orderline.export_for_printing.apply(this, arguments);
             line.grade = this.get_product().grade;
-            console.log(line.grade);
+            line.rating = this.get_product().rating;
+//            console.log(line.grade);
+//            console.log(line.rating,"ratinggg");
             return line;
         }
     });
